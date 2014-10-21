5d598f975b33840edc0f0ee3aaf0ccb4a8316029
Tzachi Dar <tzachi.dar@gmail.com>
2014-10-21 23:46:14 +0300

    Add a command line code to get the data using wifi.
    Signed-off-by: Tzachi Dar <tzachi.dar@gmail.com>

---
+++ CmdLine/ReadData.java
@@ -0,0 +1,73 @@
+import java.net.*;
+import java.util.List;
+import java.io.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+class ReadData {
+    public static void main(String[] args) {       
+        Read("192.168.1.25", 50005);
+    }
+
+
+    public static boolean Read(String hostName,int port)
+    {
+        try
+        {
+
+            Gson gson = new GsonBuilder().create();
+
+            // An example of using gson.
+            ComunicationHeader ch = new ComunicationHeader();
+            ch.version = 1;
+            ch.numberOfRecords = 3;
+            String flat = gson.toJson(ch);
+            ComunicationHeader ch2 = gson.fromJson(flat, ComunicationHeader.class);  
+            System.out.println("Results code" + flat + ch2.version);
+
+
+            // Real client code
+            Socket MySocket = new Socket(hostName, port);
+
+            System.out.println("After the new socket \n");
+            MySocket.setSoTimeout(2000); 
+                     
+            System.out.println("client connected... " );
+            
+            PrintWriter out = new PrintWriter(MySocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(MySocket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+            out.println(flat);
+            
+            while(true) {
+                String data = in.readLine();
+                if(data == null) {
+                    System.out.println("recieved null exiting");
+                    break;
+                }
+                if(data.equals("")) {
+                    System.out.println("recieved \"\" exiting");
+                    break;
+                }
+
+                System.out.println( "data size " +data.length() + " data = "+ data);
+                TransmitterRawData trd = gson.fromJson(data, TransmitterRawData.class);
+                System.out.println( trd.toTableString());
+            }
+
+
+            MySocket.close();
+            return true;
+        }catch(SocketTimeoutException s) {
+            System.out.println("Socket timed out!...");
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+            System.out.println("cought exception " + e.getMessage());
+        }
+        return false;
+   }
+}
