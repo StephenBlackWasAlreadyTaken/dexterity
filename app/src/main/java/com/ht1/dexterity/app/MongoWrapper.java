7092d053c51bdc8e04ec62f212992486fab831ef
Stephen <stephen@shopkeep.com>
2015-01-19 23:21:28 -0500

    make it build easily

--- app/src/main/java/com/ht1/dexterity/app/MongoWrapper.java
+++ app/src/main/java/com/ht1/dexterity/app/MongoWrapper.java
@@ -10,6 +10,7 @@ import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
 import com.mongodb.MongoClientURI;
+import com.ht1.dexterity.app.ReadData;
 
 import java.util.Date;
 import java.util.LinkedList;
@@ -17,35 +18,35 @@ import java.util.List;
 import java.util.Set;
 
 public class MongoWrapper {
-	
-	
-	
+
+
+
 /*
- old start code 
-	
+ old start code
+
 public static void main(String[] args) {
     MongoClientURI dbUri = new MongoClientURI("mongodb://tzachi_dar:tzachi_dar@ds053958.mongolab.com:53958/nightscout");
     System.out.println( "Starting");
     try {
-        
+
         MongoClient mongoClient = new MongoClient(dbUri);
         DB db = mongoClient.getDB( "nightscout" );
-        
+
         DBCollection coll = db.getCollection("try1");
-        
+
         coll.createIndex(new BasicDBObject("i", 1));  // create index on "i", ascending
 
-        
+
         for (int i =0; i < 10; i++) {
             BasicDBObject doc = new BasicDBObject("name", "MongoDB")
             .append("type", "database")
             .append("count", i);
-            
+
             coll.insert(doc);
         }
 
-        
-        
+
+
         DBCursor cursor = coll.find();
         try {
            while(cursor.hasNext()) {
@@ -54,10 +55,10 @@ public static void main(String[] args) {
         } finally {
            cursor.close();
         }
-        
+
         System.out.println("Now with index...");
         System.out.println("===================");
-        
+
         DBObject query = new BasicDBObject();
         cursor = coll.find(query);
         cursor.sort(new BasicDBObject("count", -1));
@@ -68,11 +69,11 @@ public static void main(String[] args) {
          } finally {
             cursor.close();
          }
-        
+
         // now get the numbers that are bigger than 7
         System.out.println("Now the numbers bigger than 7");
         System.out.println("===================");
-        
+
         query = new BasicDBObject(new BasicDBObject("count", new BasicDBObject("$gt", 7)));
         cursor = coll.find(query);
         cursor.sort(new BasicDBObject("count", 1));
@@ -82,10 +83,10 @@ public static void main(String[] args) {
             }
          } finally {
             cursor.close();
-         }        
-        
-        
-        
+         }
+
+
+
         //        Set<String> colls = db.getCollectionNames();
 
 //        for (String s : colls) {
@@ -95,19 +96,19 @@ public static void main(String[] args) {
         //throw new IOException("Error connecting to mongo host " + dbUri, e);
         System.out.println( "Failed to open table");
     }
-      
+
     }
 
 */
-	
-	
+
+
 	MongoClient mongoClient_;
 	String dbUriStr_;
 	String dbName_;
 	String collection_;
 	String index_;
 	String machineName_;
-	
+
 	public MongoWrapper(String dbUriStr, String collection, String index, String machineName) {
 		dbUriStr_ = dbUriStr;
 		// dbName is the last part of the string starting with /dbname
@@ -116,21 +117,21 @@ public static void main(String[] args) {
 		index_ = index;
 		machineName_ = machineName;
 	}
-	
+
 	// Unfortunately, this also throws other exceptions that are not documetned...
     public DBCollection openMongoDb() throws UnknownHostException {
 
     	MongoClientURI dbUri = new MongoClientURI(dbUriStr_); //?? thros
 	    mongoClient_ = new MongoClient(dbUri);
-	    
+
 	    DB db = mongoClient_.getDB( dbName_ );
 	    DBCollection coll = db.getCollection(collection_);
 	    coll.createIndex(new BasicDBObject(index_, 1));  // create index on "i", ascending
-	    
+
 	    return coll;
-	 
+
     }
-     
+
      public void closeMongoDb() {
          if(mongoClient_ != null) {
     	 	mongoClient_.close();
@@ -144,13 +145,13 @@ public static void main(String[] args) {
     	 return WriteToMongo(doc);
      }
 
-     
+
      public boolean WriteToMongo(TransmitterRawData trd)
      {
     	 BasicDBObject bdbo = trd.toDbObj(machineName_ + " " + new Date(trd.CaptureDateTime).toLocaleString());
     	 return WriteToMongo(bdbo);
      }
-     
+
      public boolean WriteToMongo(BasicDBObject bdbo)
      {
      	DBCollection coll;
@@ -160,25 +161,25 @@ public static void main(String[] args) {
 
  		} catch (UnknownHostException e) {
  			e.printStackTrace();
- 			return false; 
+ 			return false;
  		} catch (MongoException e) {
  			e.printStackTrace();
- 			return false; 
+ 			return false;
  		} catch (Exception e) {
  			e.printStackTrace();
  			closeMongoDb();
- 			return false; 
+ 			return false;
  		}
      	finally {
  			closeMongoDb();
  		}
      	return true;
      }
-     
+
      // records will be marked by their timestamp
      public List<TransmitterRawData> ReadFromMongo(int numberOfRecords) {
-    	System.out.println( "Starting to read from mongodb"); 
-    	 
+    	System.out.println( "Starting to read from mongodb");
+
     	List<TransmitterRawData> trd_list = new LinkedList<TransmitterRawData>();
       	DBCollection coll;
       	TransmitterRawData lastTrd = null;
@@ -203,7 +204,7 @@ public static void main(String[] args) {
                     	trd_list.add(0,trd);
                     	System.out.println( trd.toTableString());
                     }
-                    
+
                 }
              } finally {
                 cursor.close();
@@ -212,20 +213,20 @@ public static void main(String[] args) {
   		} catch (UnknownHostException e) {
   			// TODO Auto-generated catch block
   			e.printStackTrace();
-  			return null; 
+  			return null;
   		} catch (MongoException e) {
   			// TODO Auto-generated catch block
   			e.printStackTrace();
-  			return trd_list; 
+  			return trd_list;
   		} catch (Exception e) {
  			e.printStackTrace();
  			closeMongoDb();
- 			return null; 
+ 			return null;
  		}finally {
   			closeMongoDb();
   		}
       	return trd_list;
-    	 
+
      }
 
 
